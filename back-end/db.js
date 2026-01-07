c117becc5b77e106be6f32da0007d107bd63e012
Andrujus <justasandrusk@gmail.com>
2026-01-07 12:10:42 +0200

    updated db

--- back-end/db.js
+++ back-end/db.js
@@ -1,12 +1,18 @@
 import pkg from "pg"
 const { Pool } = pkg;
+import "dotenv/config";
+
 
 const pool = new Pool({
-    user: "andrujus",
-    password: "",
-    host: "localhost",
+    user: "postgres.dhnfukswupijdrbzpqpi",
+    password: process.env.SUPABASE_DB_PASSWORD,
+    host: "aws-1-eu-north-1.pooler.supabase.com",
     port: 5432,
-    database: "enebatechtask"
+    database: "postgres",
+    ssl: {
+        require: true,
+        rejectUnauthorized: false,
+    },
 });
 
 export default pool
